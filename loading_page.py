076922082bc2d6e8cb2b0ec744f477dcbd69e96e
MyPad <jsncastillo712@gmail.com>
2023-09-03 00:34:01 +0800

    Abstracted the css generation to separate script

--- loading_page.py
+++ loading_page.py
@@ -1,31 +1,25 @@
 import os
-import image_loader as image
-import streamlit as stream
+import streamlit as st
 import config as cfg
 
-from page_activity_data import PageActivityData
+from scripts.css_recipe import CSSRecipe
+from scripts.page_activity_data import PageActivityData
 
-manual_style        = """
-<style>
-body {
-    background-image: url("data:image/png;base64,app/static/loading_page.png");
-    background-size: cover;
-}
-</style>
-"""
+css_recipe          = CSSRecipe("static/loading_page.png")
 
-stream.set_page_config(
+st.set_page_config(
     page_title  = cfg.loading_screen.page_title,
     page_icon   = cfg.loading_screen.page_icon,
     layout      = cfg.loading_screen.page_layout,
 )
 
-stream.markdown(
-    manual_style,
+st.markdown(
+    str(css_recipe),
     unsafe_allow_html=True,
 )
 
-# with stream.container():
-#     stream.write("---")
-#     stream.write("hax")
-#     stream.write("##")
+st.button(
+    label       ="Submit",
+    type        ="primary",
+    on_click    = lambda: st.write("U smell like cow farts."),
+)
---
+++ scripts/image_loader.py
@@ -0,0 +1,11 @@
+import base64
+
+# Ripped this snippet from the link provided below:
+# https://www.youtube.com/watch?v=pyWqw5yCNdo (Timestamp: 3:58)
+def load64(file: str):
+    '''
+    Loads an image into the web application.
+    '''
+    with open(file, "rb") as filestream:
+        data    = filestream.read()
+    return base64.b64encode(data).decode()
