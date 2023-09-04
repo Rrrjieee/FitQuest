f6caebf5a88e12be5f2bb67f269794e8d42bd7b3
MyPad <jsncastillo712@gmail.com>
2023-09-04 23:43:40 +0800

    Added 3rd webpage.

--- main_page.py
+++ main_page.py
@@ -1,14 +1,31 @@
 import os
 import config as cfg
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from scripts.page_activity_data import PageActivityData
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def main_page():
-    return render_template("html/main_page.html")
+    if request.method == 'GET':
+        return render_template("main_page.html")
+
+    if request.form.get('start') == 'START':
+        return redirect("/user_list")
+
+@app.route("/user_list", methods=["GET", "POST"])
+def user_page():
+    if request.method == 'GET':
+        print("/user_list >> GET")
+        return render_template("user_select.html")
+    
+    print("/user_list >> POST")
+    return render_template("user_select.html")
+
+@app.route("/user_routine")
+def user_routine():
+    return render_template("user_routine_prompt.html")
 
 if __name__ == "__main__":
     app.run()
